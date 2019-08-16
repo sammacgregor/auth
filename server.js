b514519c93839cce9b937fd4a52ce2a4e472f4f9
Sam MacGregor <sammacgregor59@gmail.com>
2019-08-16 22:20:41 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+
+var passport = require("./config/passport");
+
+var PORT = process.env.PORT || 3002;
+var db = require("./models");
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT %s. Visit http://localhost:%s in your browser ", PORT, PORT);
+    });
+
+});
